83d4d336c8bb42466e4bb593cc75d47145be505b
Cristhian Cintra <64995266+CintraB@users.noreply.github.com>
2025-03-04 15:57:46 -0300

    Feat: adicionando CORS
    Adicionada configuração do cors e mudando servidor para rodar na porta 8080.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,12 @@
+require("dotenv").config();
 const express = require("express");
 const routes = require("./routes/index.js");
+const cors = require ("cors");
 
 const app = express();
+app.use(cors({
+    origin: process.env.ENABLE_CORS?.split(';') || []
+}));
 app.use(express.json());
 
 
